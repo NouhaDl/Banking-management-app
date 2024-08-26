7fe3cd7f801397aa3abab4f85561f55b577fa835
NouhaDl <140903697+NouhaDl@users.noreply.github.com>
2024-08-26 23:57:52 +0100

    refact:Bankstat

--- src/main/java/com/example/Project2/servicesimpl/UserServiceImpl.java
+++ src/main/java/com/example/Project2/servicesimpl/UserServiceImpl.java
@@ -1,10 +1,46 @@
 package com.example.Project2.servicesimpl;
 
-import com.example.Project2.dto.*;
 import com.example.Project2.repository.UserRepository;
+import com.example.Project2.dto.*;
+import com.example.Project2.entity.Role;
+import com.example.Project2.entity.User;
+import com.example.Project2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
-private final UserRepository userRepository;
-private final
-public class UserServiceIImpl {
-v
+@Service
+
+public class  UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    TransactionService transactionService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+  @Override
+    public BankResponse createAccount(UserRequest userRequest){
+      if(userRepository.existsByEmail(userRequest.getEmail())){
+          return new BankResponse().builder()
+                  .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
+      }
+  }
+
+
+
 }
